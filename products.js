d4d421b07085eb65f434d00c3d84449c749e1525
Aishwarya30092003 <131292804+Aishwarya30092003@users.noreply.github.com>
2024-02-23 15:05:56 +0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,48 @@
+let products = [
+    {
+        id: 1001,
+        title: "Cat Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresistable</em>, " + "organic goodness for your cat",
+        price: 20.00,
+        image: "cat.jpg",
+        availableInventory: 6,
+        rating: 3
+    },
+    {
+        id: 1002,
+        title: "Dog Food, 25lb bag",
+        description: "organic goodness for your dog",
+        price: 30.00,
+        image: "dog.jpg",
+        availableInventory: 4,
+        rating: 2
+    },
+    {
+        id: 1003,
+        title: "Bird Food, 25lb bag",
+        description: "organic goodness for your bird",
+        price: 35.00,
+        image: "bird.jpg",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 1004,
+        title: "Fish Food, 25lb bag",
+        description: "organic goodness for your fish",
+        price: 25.00,
+        image: "fish.jpg",
+        availableInventory: 5,
+        rating: 3
+    },
+    {
+        id: 1005,
+        title: "Hamster Food, 25lb bag",
+        description: "organic goodness for your hamster",
+        price: 40.00,
+        image: "hamster.jpg",
+        availableInventory: 6,
+        rating: 4
+    },
+]
+
